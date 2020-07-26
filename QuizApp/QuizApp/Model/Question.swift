80a5fbc994417da0030535ebaaaa10be871ea7a2
Puneet Mahali <iospuneet@gmail.com>
2020-07-27 00:27:32 +0200

    Completed with MVC.

---
+++ QuizApp/QuizApp/Model/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Puneet Mahali on 27.07.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    
+    //Multiple choice questions have multiple answers, an Array of Strings would work for our quiz data.
+    let answers: [String]
+    //Look at the data in the quiz array, there is a seperate string that is the correctAnswer.
+    let rightAnswer: String
+    
+    //The initialiser needs to be updated to match the new multiple choice quiz data.
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        rightAnswer = correctAnswer
+    }
+}
